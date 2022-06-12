8d79ddedfc6a8831a541db5b0b055827743923bb
CrisCT <crisct@gmail.com>
2022-06-12 18:18:51 +0200

    Ej2Listas

---
+++ Ej2Listas/index.js
@@ -0,0 +1,13 @@
+const listaDatos = {
+    nombre: "Cristina",
+    edad: 26,
+    developer: true,
+    nacimiento: new Date("11, September, 1995"),
+    libro: {
+        titulo: "Mistborn",
+        autor: "Brandon Sanderson" ,
+        fecha: new Date("17, July, 2006"),
+        url: "https://www.brandonsanderson.com/",
+    }
+};
+
