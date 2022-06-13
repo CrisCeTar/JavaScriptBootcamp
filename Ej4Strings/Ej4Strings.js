349a969315532cd97d1a91300f5fe686fc59d51b
CrisCT <crisct@gmail.com>
2022-06-13 17:05:07 +0200

    Ej4Strings

---
+++ Ej4Strings/Ej4Strings.js
@@ -0,0 +1,13 @@
+nombre = "Cristina";
+apellido = "Cerro";
+estudiante = nombre + " " + apellido;
+estudianteMayus = estudiante.toUpperCase()
+estudianteMinus = estudiante.toLowerCase()
+let numLetras = estudiante.length
+let primLetra = nombre[0]
+let ultLetra = apellido.charAt(apellido.length-1) ;
+no_space = estudiante.trim();
+let incluyeNombre = estudiante.includes(nombre)
+
+
+
