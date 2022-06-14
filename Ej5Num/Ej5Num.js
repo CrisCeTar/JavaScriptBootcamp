03a2634083615a4fc1fdea9028692d19056d14c4
CrisCT <crisct@gmail.com>
2022-06-14 12:28:42 +0200

    Ej5Num

---
+++ Ej5Num/Ej5Num.js
@@ -0,0 +1,10 @@
+
+let alturaCm = 160;
+let alturaM = 1.60;
+let pesoKg = 59.5;
+let alturaRed = alturaM.toFixed();
+let pesoRed = Math.floor(pesoKg);
+let maxValor = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+
+console.log(maxValor)
+
