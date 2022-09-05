637d068b50709c8f7737d9359d430ded2285c160
CrisCT <crisct@gmail.com>
2022-09-05 17:49:46 +0200

    ejercicioFunciones

---
+++ Ej8Funciones/ejercicioFunciones.js
@@ -0,0 +1,41 @@
+function siempreTrue() {
+    return true;
+}
+
+console.log(siempreTrue())
+
+////////////////////////////////
+
+
+function myAsinc() {
+  timeOut = setTimeout(mensahe, 5000);
+}
+
+function mensahe() {
+  console.log("Hola soy una promesa");
+}
+
+myAsinc()
+
+////////////////////////////////
+
+function* generarId() {
+    let id = 2;
+    while (id < 2) {
+        id++
+        if (id % 2 == 0) {
+            return id
+        }
+        yield id
+    }
+}
+
+const gen = generarId();
+
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+generarIdPar()
